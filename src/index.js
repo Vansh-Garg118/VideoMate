d979d0433dd54c801f654b7d27524477182853ab
Vansh Garg <vanshgarg25804@gmail.com>
2024-07-02 23:53:33 +0530

    db connection to mongodb

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv"
+
+
+import mongoose from "mongoose";
+import {DB_NAME} from './constants.js'
+import express from "express"
+
+dotenv.config({
+    path:'./env'
+})
+const MONGODB_URI="mongodb+srv://vanshgarg25804:DqRspf8B2iQ44JSW@cluster0.oazlwol.mongodb.net"
+const app=express();
+;(async ()=>{
+    try {
+        const dbres=await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`)
+
+        console.log(dbres);
+        app.on("error",(error)=>{
+            console.log("EXPress error::",error)
+            throw error
+        })
+
+        app.listen(process.env.PORT||3000,()=>{
+            console.log(`app is listening at ${process.env.PORT||3000}`)
+        })
+        
+    } catch (error) {
+        console.log("ERROR on db connection::",error)
+    }
+})()
